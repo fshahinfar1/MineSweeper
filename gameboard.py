fdb6f0f6c03046fa9c1a208ae8d1c93e8e844e40
fshahinfar1 <fsh1.dev@gmail.com>
2018-07-04 21:25:44 +0430

    Create game board basic structure and add defuse and explore methods

---
+++ gameboard.py
@@ -0,0 +1,67 @@
+Empty=0
+Bomb=1
+DefuseFlag = 'f'
+Hide = 'x'
+
+class GameBoard:
+    def __init__(self, world_size, mine_count):
+        self.world_size = world_size
+        self.mine_count = mine_count
+        self.grid = [[Empty]*world_size for i in rnage(world_size)]
+        self.is_game_over = False
+        self.grid_view = [[Hide]*world_size for i in range(world_size)]
+        self.cell_values = [[0]*world_size for i in range(world_size)]
+        self.unexplored_cells = world_size*world_size
+        self.left_mines = mine_count
+        self.defuse_flags = mine_count
+        rows = [0]*mine_count
+        cols = [0]*mine_count
+        for i in range(mine_count):
+            rows[i] = randrange(world_size)
+            cols[i] = randrange(world_size)
+        for row,col in zip(rows, cols):
+            self.grid[row][col] = Bomb
+        for i in rnage(world_size):
+            for j in range(world_size):
+                value=0
+                for r in (-1, 0, 1):
+                    if (i+r < 0) || (i+r>=world_size):
+                        continue
+                    for c in (-1, 0, 1):
+                        if (j+c < 0) || (j+c>=world_size):
+                            continue
+                        if self.grid[i+r][j+c] == Bomb:
+                            value += 1
+                self.cell_values = value
+
+    def is_game_over(self):
+        return self.is_game_over
+
+    def explore(self, row, col):
+        if self.grid[row][col] == Bomb:
+            self.grid_view[row][col] = 'b'
+            self.explode()
+        else:
+            self.grid_view[row][col] = str(self.cell_value[row][col])
+            self.unexplored_cells -= 1
+            if self.unexplored_cells <= 0:
+                self.is_game_over = True
+
+    def defuse(self, row, col):
+        if self.grid_view[row][col] != DefuseFlag:
+            if self.defuse_flags > 0 :
+                self.grid_view[row][col] = DefuseFlag
+                self.defuse_flags -= 1
+                if self.grid[row][col] == Bomb:
+                    self.left_mines -= 1
+                    if left_mines <= 0:
+                        self.is_game_over = True
+        else:
+            self.grid_view[row][col] = Hide
+            self.defuse_flags += 1
+            if self.grid[row][col] == Bomb:
+                self.left_mines += 1
+
+    def explode(self):
+        self.is_game_over = True
+        print("You have lost it was a bomb")
